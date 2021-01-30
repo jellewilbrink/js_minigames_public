727b2b49172a31d5ef157fac030334ec813c1663
MrHaan <49995469+MrHaan@users.noreply.github.com>
2021-01-30 17:44:23 +0100

    testversion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const code_version="1.0";
-console.log("loaded the index.js file!!!");
-window.onload=()=>{
+
+window.onload=function(){
     //register the serviceworker
     if('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./js_minigames_public/sw.js');
@@ -8,4 +8,4 @@ window.onload=()=>{
 
     //display the current code version
     console.log("Site version: " + code_version);
-}
+};
