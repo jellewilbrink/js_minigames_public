f2287390f5382d57833c501b2342187466844e37
MrHaan <49995469+MrHaan@users.noreply.github.com>
2021-01-30 17:47:35 +0100

    testversion

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const code_version="1.0";
 
-window.onload=function(){
-    //register the serviceworker
-    if('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./js_minigames_public/sw.js');
-    };
+//display the current code version
+console.log("Site version: " + code_version);
 
-    //display the current code version
-    console.log("Site version: " + code_version);
+//register the serviceworker
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./js_minigames_public/sw.js');
 };
