9177b14e11149f91421e21c84477a876329ed516
MrHaan <49995469+MrHaan@users.noreply.github.com>
2021-01-30 17:34:48 +0100

    linked index.js to register sw.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//register the serviceworker
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./js_minigames_public/sw.js');
-};
+const code_version="1.0";
+
+window.onload=()=>{
+    //register the serviceworker
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./js_minigames_public/sw.js');
+    };
+
+    //display the current code version
+    console.log("Site version: " + code_version);
+}
