310bfca6f3d97a2769aee56f6fe55b2fc80d0a68
MrHaan <49995469+MrHaan@users.noreply.github.com>
2021-01-30 17:49:57 +0100

    testversion - bug fix index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ console.log("Site version: " + code_version);
 
 //register the serviceworker
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./js_minigames_public/sw.js');
+    navigator.serviceWorker.register('sw.js');
 };
